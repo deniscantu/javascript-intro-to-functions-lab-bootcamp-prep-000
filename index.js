bfc9f6363baa5baea5215f5509f6d08db8d097a5
Denis <denis@comebien.mx>
2017-12-02 16:02:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,13 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+      return "YES INDEED!"
+  } else {
+      return "I love you, too."
+    }
   }
-}
+      
 
-function sayHiToGrandma(string) {
-  if (string === string.toUpperCase()) {
-    return "YES INDEED!"
-  }
-}
+
+
